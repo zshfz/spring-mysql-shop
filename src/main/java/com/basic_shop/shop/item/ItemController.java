efc0afb3be105fb1f8fc1fdc66e68edeca7f69b5
zshfz <zshfz8634@gmail.com>
2025-05-19 10:19:40 +0900

    feat 상품 등록 api, NAV&FOOTER 추가

--- src/main/java/com/basic_shop/shop/item/ItemController.java
+++ src/main/java/com/basic_shop/shop/item/ItemController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -11,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -18,4 +22,24 @@ public class ItemController {
         model.addAttribute("items", result);
         return "list.html";
     }
+
+    @GetMapping("/write")
+    public String list() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    public String addPost(@RequestParam String name, @RequestParam Integer price) {
+        itemService.saveItem(name, price);
+        return "redirect:/list";
+    }
+
+    //위에 처럼 안하고 이렇게 해도 됨
+   /*
+   @PostMapping("/add")
+    public String addPost(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+    */
 }
--- src/main/java/com/basic_shop/shop/item/ItemService.java
+++ src/main/java/com/basic_shop/shop/item/ItemService.java
@@ -1,4 +1,18 @@
 package com.basic_shop.shop.item;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public void saveItem(String name, Integer price) {
+        Item item = new Item();
+        item.setName(name);
+        item.setPrice(price);
+        itemRepository.save(item);
+    }
 }

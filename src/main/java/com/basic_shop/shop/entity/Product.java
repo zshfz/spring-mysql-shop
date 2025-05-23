72b2764ff3dcc5e74507fe0633d95d822322456b
zshfz <zshfz8634@gmail.com>
2025-05-23 09:41:20 +0900

    chore h2 db 연결 설정

---
+++ src/main/java/com/basic_shop/shop/entity/Product.java
@@ -0,0 +1,19 @@
+package com.basic_shop.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private Integer price;
+}

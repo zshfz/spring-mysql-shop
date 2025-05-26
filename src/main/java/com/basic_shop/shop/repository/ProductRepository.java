4bd1e18d74671aca8b6f373c8e591f95e7603a4f
zshfz <zshfz8634@gmail.com>
2025-05-26 17:39:25 +0900

    chore: reset main branch to empty

--- src/main/java/com/basic_shop/shop/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.basic_shop.shop.repository;
-
-import com.basic_shop.shop.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
